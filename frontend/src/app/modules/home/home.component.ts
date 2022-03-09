e887ab3b94f8c836b13566c550348bed29be4612
Radim Staněk <stanek.radim@gtomy.eu>
2022-03-09 17:05:43 +0100

    Basic Google Maps integration

--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -5,4 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  title = 'firstApp';
+
+  lat = 22.4064172;
+  long = 69.0750171;
+  zoom = 7;
+
+  markLat = 21.1594627;
+  markLng = 72.6822083;
+
+  markerDragEnd($event: any): void {
+    console.log($event);
+    this.markLat = $event.coords.lat;
+    this.markLng = $event.coords.lng;
+  }
+
+  printMarkerInfo(): void {
+    console.log(this.markLat);
+    console.log(this.markLng);
+  }
+}
--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { AgmCoreModule } from '@agm/core';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, RouterModule],
+  imports: [
+    SharedModule,
+    RouterModule,
+    AgmCoreModule.forRoot({
+      apiKey: '',
+    }),
+    CommonModule,
+  ],
   exports: [],
 })
 export class HomeModule {}
