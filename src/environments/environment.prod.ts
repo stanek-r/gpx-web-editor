4374696c8356c59cf9312ad6504a6f8dec0d6e80
Radim Staněk <stanek@gtomy.com>
2023-03-25 23:12:36 +0100

    License and better deploy 9

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB1u-WpD9UKVInSv7mrEbPC1DOdK0cUaec',
+    authDomain: 'gpx-web-editor.web.app',
+    databaseURL: 'https://gpx-web-editor-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: 'gpx-web-editor',
+    storageBucket: 'gs://gpx-web-editor.appspot.com',
+  },
+};
