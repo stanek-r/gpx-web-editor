c66df40e51cfae346dbd7008b4bf142ab7c436f7
Radim Staněk <stanek@gtomy.com>
2023-05-04 21:10:18 +0200

    Run test on change 2

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-translate', '--disable-extensions']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
